0627df70572729a73b0306031a8788c11f30fb49
Gilbert Thiodores <gthiodores71@gmail.com>
2022-03-15 18:13:02 +0800

    Specify interface in api service tests

--- test/api_service_test.dart
+++ test/api_service_test.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:restaurant_app/core/data/api/api_service_interface.dart';
 
 import 'fakes/fake_api_service.dart';
 
 void main() {
   group('Api Service Testing', () {
     test('Get Restaurant List From Api Returns Restaurant List', () async {
-      final apiService = FakeApiService();
+      final IApiService apiService = FakeApiService();
 
       final restaurantLists = await apiService.getRestaurantList();
 
@@ -13,23 +14,25 @@ void main() {
     });
 
     test('Get Restaurant Detail From Api Returns Restaurant Detail', () async {
-      final apiService = FakeApiService();
+      final IApiService apiService = FakeApiService();
 
       final restaurantDetail = await apiService.getRestaurant("");
 
       expect(restaurantDetail.name, "Melting Pot");
     });
 
-    test('Get Search Result From Api Returns Restaurant Search Result', () async {
-      final apiService = FakeApiService();
+    test('Get Search Result From Api Returns Restaurant Search Result',
+        () async {
+      final IApiService apiService = FakeApiService();
 
-      final restaurantSearch = await apiService.getRestaurantSearchResult("test");
+      final restaurantSearch =
+          await apiService.getRestaurantSearchResult("test");
 
       expect(restaurantSearch.restaurants.isNotEmpty, true);
     });
 
     test('Get Random Endpoint Returns Error', () async {
-      final apiService = FakeApiService();
+      final IApiService apiService = FakeApiService();
 
       final exception = apiService.getRestaurantSearchResult("query");
 
